a9acdaf2068961c032270b6191e0226c09b01321
Vladimir Shvoev <shvoev.work@gmail.com>
2021-12-06 17:08:50 +0700

    Added temp button logic

--- Assets/Scripts/ButtonLogicScript.cs
+++ Assets/Scripts/ButtonLogicScript.cs
@@ -5,10 +5,20 @@ using UnityEngine.Tilemaps;
 
 public class ButtonLogicScript : MonoBehaviour
 {
+    /// <summary>
+    /// Turn left.
+    /// </summary>
+	private int? _turnLeft;
+
 	/// <summary>
     /// Tilemap current game object.
     /// </summary>
 	private Tilemap _thisTilemap;
+
+    /// <summary>
+    /// Button coordinate.
+    /// </summary>
+	private Vector3Int _buttonCoordinate;
 
     /// <summary>
     /// Trap tile map.
@@ -30,16 +40,50 @@ public class ButtonLogicScript : MonoBehaviour
     /// </summary>
     public TileBase UnpressedButton;
 
+    /// <summary>
+    /// Trap turn off count turn
+    /// </summary>
+    public int TrapTurnOffCountTurn = -1;
+
     // Start is called before the first frame update
     void Start()
     {
         _thisTilemap = gameObject.GetComponent<Tilemap>();
+        var mainObject = GameObject.FindGameObjectWithTag("MainObject");
+        var mainScript = mainObject.GetComponent<MainScript>();
+        if (mainScript != null)
+        {
+	        mainScript.TurnChanged.AddListener(OnTurnChanged);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 	    CheckPressButton();
+    }
+
+    /// <summary>
+    /// Turn changed handle.
+    /// </summary>
+    private void OnTurnChanged()
+    {
+	    if (_turnLeft < 0)
+	    {
+		    return;
+	    }
+
+	    var trapActivateScript = TrapTileMap.GetComponent<TrapActivateScript>();
+	    if (_turnLeft > 0 &&
+	        !trapActivateScript.IsActive)
+	    {
+		    _turnLeft--;
+	    }
+	    else if (!trapActivateScript.IsActive)
+	    {
+		    trapActivateScript.IsActive = true;
+		    _thisTilemap.SetTile(_buttonCoordinate, UnpressedButton);
+	    }
     }
 
     /// <summary>
@@ -54,11 +98,13 @@ public class ButtonLogicScript : MonoBehaviour
 		    return;
 	    }
 
-	    _thisTilemap.SetTile(playerCoordinate, PressedButton);
+	    _buttonCoordinate = playerCoordinate;
+	    _thisTilemap.SetTile(_buttonCoordinate, PressedButton);
 	    var trapActivateScript = TrapTileMap.GetComponent<TrapActivateScript>();
 	    if (trapActivateScript != null && trapActivateScript.IsActive)
 	    {
 		    trapActivateScript.IsActive = false;
+		    _turnLeft = TrapTurnOffCountTurn;
 	    }
     }
 }
