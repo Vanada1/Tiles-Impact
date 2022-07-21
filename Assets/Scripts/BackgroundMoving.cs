3955693afaa35398c37225fd4bdc6518fd953897
Vladimir Shvoev <56462847+Vanada1@users.noreply.github.com>
2022-07-21 22:04:00 +0700

    animated background

---
+++ Assets/Scripts/BackgroundMoving.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoving : MonoBehaviour
+{
+    private float _offset;
+
+    private Material _material;
+
+	[Range(-1f, 1f)]
+    public float ScrollSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var render = GetComponent<Renderer>();
+        _material = render.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _offset += (Time.deltaTime * ScrollSpeed) / 10f;
+        _material.SetTextureOffset("_MainTex", new Vector2(_offset, 0));
+    }
+}
